b8d1965683b0500230f6b38e6978dd55fe28bd13
OkithM <okithmoksha@gmail.com>
2026-01-13 22:33:46 +0530

    create admin form & news get request

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,14 @@ const app = express();
 const PORT = 3000;
 
 
-let news =[{title:"title1", content:"content1",date:new Date("2025-12-25")}, {title:"title2", content:"content2",date:new Date("2025-11-25")}, {title:"title3", content:"content3",date:new Date("2025-10-25")}];
+let news =[{title:"title1", content:"content1 lore ipsum dolor sit amet lore ipsum dolor sit amet lore ipsum dolor sit amet ",date:new Date("2025-12-25")}, {title:"title2", content:"content2 content1 lore ipsum dolor sit amet lore ipsum dolor sit amet lore ipsum dolor sit amet ",date:new Date("2025-11-25")}, {title:"title3", content:"content3 content1 lore ipsum dolor sit amet lore ipsum dolor sit amet lore ipsum dolor sit amet ",date:new Date("2025-10-25")}];
 
 // Middleware to parse JSON data
 app.use(express.json());
 
 const cors = require('cors');
+
+app.use(express.static('public'));
 
 app.use(
   cors({
---
+++ backendTest/script.js
@@ -0,0 +1,17 @@
+var latestnews = [];
+
+fetch("http://localhost:3000/latestnews", {
+    method: "GET",
+})
+    .then(response => response.json())
+    .then(data => {
+        latestnews = data;
+        console.log(latestnews);
+
+        for (let i = 0; i < 3; i++) {
+        const newsHTML = `<div class="notice"><img src="http://localhost:3000/images/${latestnews[i].title}.jpg"><p class="miniheading">${latestnews[i].title}<br></p><p class="description">${latestnews[i].content}</p><button type="button" class="readmore">Read More</button></div>`;
+        document.body.innerHTML += newsHTML;
+        }
+
+
+    })
