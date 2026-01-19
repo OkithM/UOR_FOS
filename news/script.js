46735964089e7d60f6c5bf4bb187d6c0c4f9c576
OkithM <okithmoksha@gmail.com>
2026-01-19 22:00:27 +0530

    news-backend

---
+++ news/script.js
@@ -0,0 +1,46 @@
+
+fetch('./../menu.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('menu').innerHTML = data;
+        activePage = document.getElementById("pagename_news");
+        activePage.style.color = "#003366";
+        activePage.style.fontWeight = "bold";
+    });
+fetch('./../footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer').innerHTML = data;
+    });
+
+function showpagename() {
+    document.getElementById("pageslist").style.display = "flex";
+}
+
+
+
+
+const newsGrid = document.getElementById("news-grid");
+
+var latestnews = [];
+const serverUrl = "http://localhost:3000";
+var dots = "";
+
+fetch(`${serverUrl}/latestnews`, {
+  method: "GET",
+})
+  .then((response) => response.json())
+  .then((data) => {
+    latestnews = data;
+    console.log(latestnews);
+
+    for (let i = 0; i < 3; i++) {
+      if (latestnews[i].title.length > 50) {
+        dots = "...";
+      } else {
+        dots = "";
+      }
+      var news = `<div class="news"><h3 style="color: #002147;">${latestnews[i].title.slice(0, 50)}${dots}</h3><div class="news-img" style="background-image: url(${serverUrl}/${latestnews[i].image_path});"  ></div> <p class="description">${latestnews[i].content.slice(0, 100)}...</p><button> <a href="https://www.ruh.ac.lk/index.php/en/news/895-the-32nd-convocation-2025" target="_blank">Read More</a> </button></div>`
+      newsGrid.innerHTML += news;
+    }
+  });   
